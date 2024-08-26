6597f9f930ba485b0c51a835ace6f74a330838bc
Cristiano Rodrigues Alves da Silva <cristiano.silva@gruporihappy.com.br>
2024-08-25 21:25:40 -0300

    feat: now properly save .xls file

--- vbs.py
+++ vbs.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def create_vbs_script(file_path, username, password, transaction, skus, store, start_date, end_date, export_path, export_file):
+def create_vbs_script(file_path, username, password, transaction, skus, store, start_date, end_date, export_path, filename):
     vbs_script = f'''
 If Not IsObject(application) Then
    Set SapGuiAuto  = GetObject("SAPGUI")
@@ -30,24 +30,27 @@ session.findById("wnd[0]/usr/btn%_S_MATNR_%_APP_%-VALU_PUSH").press
         vbs_script += f'session.findById("wnd[1]/usr/tabsTAB_STRIP/tabpSIVA/ssubSCREEN_HEADER:SAPLALDB:3010/tblSAPLALDBSINGLE/ctxtRSCSEL-SLOW_I[1,{i}]").text = "{sku}"\n'
     
     vbs_script += f'''
-session.findById("wnd[1]/tbar[0]/btn[0]").press
-session.findById("wnd[1]/tbar[0]/btn[8]").press
-session.findById("wnd[0]/usr/ctxtS_WERKS-LOW").text = "{store}"
-session.findById("wnd[0]/usr/ctxtS_DATUM-LOW").text = "{start_date}"
-session.findById("wnd[0]/usr/ctxtS_DATUM-HIGH").text = "{end_date}"
-session.findById("wnd[0]").sendVKey 0
-session.findById("wnd[0]/tbar[1]/btn[8]").press
-session.findById("wnd[0]/tbar[1]/btn[26]").press
-session.findById("wnd[0]/tbar[1]/btn[38]").press
-session.findById("wnd[0]/tbar[1]/btn[45]").press
-session.findById("wnd[1]/usr/ctxtDY_PATH").text = "{export_path}"
-session.findById("wnd[1]/usr/ctxtDY_FILENAME").text = "{export_file}"
-session.findById("wnd[1]").sendVKey 0
-session.findById("wnd[0]/tbar[0]/okcd").text = "/n/n/n/n/n"
-session.findById("wnd[0]").sendVKey 0
+    session.findById("wnd[1]/tbar[0]/btn[0]").press
+    session.findById("wnd[1]/tbar[0]/btn[8]").press
+    session.findById("wnd[0]/usr/ctxtS_WERKS-LOW").text = "{store}"
+    session.findById("wnd[0]/usr/ctxtS_DATUM-LOW").text = "{start_date}"
+    session.findById("wnd[0]/usr/ctxtS_DATUM-HIGH").text = "{end_date}"
+    session.findById("wnd[0]/tbar[1]/btn[8]").press 'execution f9
+    session.findById("wnd[0]/tbar[1]/btn[26]").press 'normal view
+    session.findById("wnd[0]/tbar[1]/btn[38]").press 'grid control
+    session.findById("wnd[0]/tbar[1]/btn[45]").press 'export file
+    session.findById("wnd[1]/usr/subSUBSCREEN_STEPLOOP:SAPLSPO5:0150/sub:SAPLSPO5:0150/radSPOPLI-SELFLAG[1,0]").select 'option 1
+    session.findById("wnd[1]/usr/subSUBSCREEN_STEPLOOP:SAPLSPO5:0150/sub:SAPLSPO5:0150/radSPOPLI-SELFLAG[1,0]").setFocus 'option 2
+    session.findById("wnd[1]/tbar[0]/btn[0]").press 'confirm export as excel sheet
+    session.findById("wnd[1]/usr/ctxtDY_PATH").text = "{export_path}" 'dir path
+    session.findById("wnd[1]/usr/ctxtDY_FILENAME").text = "{filename}" 'filename
+    session.findById("wnd[1]/usr/ctxtDY_PATH").caretPosition = Len("{export_path}")
+    session.findById("wnd[1]").sendVKey 0
+    session.findById("wnd[0]/tbar[0]/okcd").text = "/n/n/n/n/n"
+    session.findById("wnd[0]").sendVKey 0
     '''
     
-    with open(file_path, 'w') as file:
+    with open(file_path, 'a') as file:
         file.write(vbs_script)
 
 def run_sap_script(script_path):
@@ -63,16 +66,16 @@ def run_sap_script(script_path):
         print(f"Ocorreu um erro: {e}")
 
 # Parâmetros do Script
-file_path = "sap_script.vbs"
-username = "seu_usuario_sap"
-password = "sua_senha_sap"
+file_path = "G:\Meu Drive\Development\sylphie\sap_script.vbs"
+username = "zigfy"
+password = "password"
 transaction = "VKP2"
-skus = ["5100695", "5062938", "5071190"]
+skus = ["5100695", "5062938", "5071190", "751790"]
 store = "1048"
 start_date = "01.08.2023"
 end_date = "31.08.2023"
 export_path = "C:\\Users\\zigfy\\Documents\\SAP\\SAP GUI\\"
-export_file = "FILE_LOCAL.XLS"
+export_file = "python2.XLS"
 
 # Gerar e executar o script VBS
 create_vbs_script(file_path, username, password, transaction, skus, store, start_date, end_date, export_path, export_file)
