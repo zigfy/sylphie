5a440fe88f3c9880cc7d1e0ba282ad1439be6f0f
Cristiano Rodrigues Alves da Silva <cristiano.silva@gruporihappy.com.br>
2024-09-08 16:14:26 -0300

    feat: all data needed is on their respective lists

--- pages/sylphie/pricing.py
+++ pages/sylphie/pricing.py
@@ -25,12 +25,25 @@ def alter_pricing():
             file_details = {"Filename": file.name, "FileType":file.type, "Size": file.size}
             st.write(file_details)
             # st.dataframe(sheet) # just for logging
-            st.write(requested['B1'].value)
 
-            # catch all 'sku sap' column
-            skus = []
-            st.write("max_rows -> ",requested.max_row)
-            st.write(getColumn_values(requested, column=f"B"))
+            # catch all needed columns
+            st.write("max_rows -> ", requested.max_row)
+            skus = getColumn_values(requested, column=f"B")
+            start_date = getColumn_values(requested, column="F")
+            end_date = getColumn_values(requested, column="G")
+            price_policy = getColumn_values(requested, column="H")
+            de_prices = getColumn_values(requested, column="I")
+            por_prices = getColumn_values(requested, column="J")
+            arred_prices = getColumn_values(requested, column="K")
+            discount_column = ((por_prices[2] / de_prices[2]) - 1) # it needs a loop to calculate formula
+            st.write(discount_column)
+            st.write(requested['B1'].value, skus)
+            st.write(requested['F1'].value, start_date)
+            st.write(requested['G1'].value, end_date)
+            st.write(requested['H1'].value, price_policy)
+            st.write(requested['I1'].value, de_prices)
+            st.write(requested['J1'].value, por_prices)
+            st.write(requested['K1'].value, arred_prices)
             # vkp2_script(file_path, username, password, transaction, skus, store, start_date, end_date, export_path, filename)
             # do vkp2 thing
             # return the prices on an array or list or something like it
