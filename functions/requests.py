e38813a6f9d88d037046d1446bff939c43f92cc8
Cristiano Rodrigues Alves da Silva <cristiano.silva@gruporihappy.com.br>
2024-11-04 19:34:36 -0300

    feat/improv: implanted ratio limit on vtex_get_prices

--- functions/requests.py
+++ functions/requests.py
@@ -1,9 +1,10 @@
 import os
+import time
 import json
 import requests
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../..', '.venv', '.env'))
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.venv', '.env'))
 vtex_api_key = os.getenv('VTEX_API_KEY')
 vtex_api_token = os.getenv('VTEX_API_TOKEN')
 price_endpoint = os.getenv('PRICE_ENDPOINT')
@@ -17,9 +18,15 @@ def vtex_get_prices(skus: list):
     'X-VTEX-API-AppToken': f"{vtex_api_token}"
     }
     res = []
-    for sku in skus:
+    for i, sku in enumerate(skus):
+
+        if i > 0 and i % 30 == 0:
+            time.sleep(1)
+
         req = requests.get(url=f'{price_endpoint}{sku}', headers=headers)
-        res.append([req.json()])
+        try: 
+            res.append([req.json()])
+        except: continue
     
     with open(json_path, 'w', encoding='utf-8') as f:
         json.dump(res, f, ensure_ascii=False, indent=2)
