d22634b2b17e5cbf56bcb90cd652c89fc5d86624
Cristiano Rodrigues <cristiano.silva@gruporihappy.com.br>
2025-01-16 18:48:56 -0300

    refeat: download diffusion xlsx from google storage using selenium

--- functions/requests.py
+++ functions/requests.py
@@ -4,6 +4,7 @@ import json
 import requests
 import smtplib
 from dotenv import load_dotenv
+from selenium import webdriver
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.venv', '.env'))
 vtex_api_key = os.getenv('VTEX_API_KEY')
@@ -51,3 +52,21 @@ def vtex_diffusion_mail():
     gmail_service.sendmail(from_addr=gmail_address, to_addrs=gmail_address, msg=msg)
     gmail_service.quit()
     return "runned."
+
+def download_file(uri, dir):
+    options = webdriver.ChromeOptions()
+    #options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_experimental_option("detach", True)
+    options.add_argument(fr"user-data-dir=C:\Users\cristiano.silva\AppData\Local\Google\Chrome\User Data\Default")
+    working_dir = {'download.default_directory' : fr'{dir}'}
+    options.add_experimental_option('prefs', working_dir)
+    driver = webdriver.Chrome(options=options, keep_alive=True)
+    driver.get(f'{uri}')
+    driver.implicitly_wait(20)
+    default_dir = {'download.default_directory': fr'C:\Users\cristiano.silva\Downloads'}
+    options.add_experimental_option('prefs', default_dir)
+    time.sleep(15)
+    driver.quit()
+
+    return True
